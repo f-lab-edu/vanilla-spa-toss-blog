4c652af4abc276c3081eeb8328a39236e6c4b2a7
정소희 <soheejung@jeongsohuiui-MacBookPro.local>
2024-06-02 19:32:18 +0900

    feat: 카테고리별 데이터 요청 추가

--- pages/page.js
+++ pages/page.js
@@ -1,23 +1,26 @@
+import articleView from './article';
+import list from './list';
+
 export default (container) => {
-  const home = () => {
-    container.innerHTML = `<div>This is Home page</div>`;
+  const home = async () => {
+    const data = await fetch('/').then((res) => res.json());
+    container.innerHTML = list(data);
   };
 
   const tech = async () => {
-    const data = await fetch('/').then((res) => res.json());
-    console.log(data);
-    container.innerHTML = data
-      .map(({ title }) => `<div>${title}</div>`)
-      .join('');
+    const data = await fetch('/tech').then((res) => res.json());
+    container.innerHTML = list(data);
   };
 
-  const design = () => {
-    container.innerHTML = `<div>This is Design page</div>`;
+  const design = async () => {
+    const data = await fetch('/design').then((res) => res.json());
+    container.innerHTML = list(data);
   };
 
-  const article = (params) => {
+  const article = async (params) => {
     const { id } = params;
-    container.textContent = `This is Article Page with Id ${id}`;
+    const data = await fetch(`/article/${id}`).then((res) => res.json());
+    container.innerHTML = articleView(data);
   };
 
   const notFound = () => {
